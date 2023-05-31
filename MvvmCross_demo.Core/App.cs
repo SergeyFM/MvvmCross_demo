1ad317fe3f5b730c6a7ca7eabf4ef85cbff74591
SergeyFM <github@svistunov.su>
2023-05-31 10:51:01 +0300

    Add project files.

---
+++ MvvmCross_demo.Core/App.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.ViewModels;
+using MvvmCross_demo.Core.ViewModels;
+
+namespace MvvmCross_demo.Core {
+    public class App : MvxApplication {
+        public override void Initialize() {
+            RegisterAppStart<GuestBookViewModel>();
+        }
+    }
+}
---
+++ MvvmCross_demo.Core/Models/PersonModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmCross_demo.Core.Models {
+    public class PersonModel {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
